e4e7b02e4c2531abbaa3af96f33e1741521fd971
Joel Rosen <joel@velarium.com>
2022-06-12 08:21:47 -0600

    Thoughful

---
+++ PlayingwithVariables/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Declare and initialize
+int myAge = 52;
+string myName = "Joel Rosen";
+char myChar = '@';
+bool isAlive = true;
+double myDouble = 209.66;
+decimal myDecimal = 315.45m;
+
+//write to console
+Console.WriteLine($"My name is {myName}, i am {myAge} years old and am I alive? {isAlive} ... my char is {myChar} and my speed is {myDecimal}");
